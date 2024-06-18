6ca929469d7013f10ae2a12c08825b1977b8cdae
Emerson <emersonsmatthias@gmail.com>
2024-06-18 17:35:19 -0300

    middleware upadted

--- src/app.middleware.ts
+++ src/app.middleware.ts
@@ -1,22 +1,27 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
-export async function AuthMiddleware(
-  req: Request & { data: UserData },
-  res: Response,
-  next: NextFunction,
-) {
-  try {
-    const JWT_SECRET_KEY = '123456789AAAAAAAA';
-    const token = req.headers.authorization.split(' ')[1];
-    const data = verify(token, JWT_SECRET_KEY);
-    req.data = JSON.parse(JSON.stringify(data));
-  } catch (error) {
-    console.log(error);
-    return res.status(400).send('Token Inválido');
-  }
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  constructor(private readonly configService: ConfigService) {}
 
-  next();
+  use(req: Request & { data: UserData }, res: Response, next: NextFunction) {
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+      const data = verify(
+        token,
+        this.configService.getOrThrow('JWT_SECRET_KEY'),
+      );
+      req.data = JSON.parse(JSON.stringify(data));
+    } catch (error) {
+      console.log(error);
+      return res.status(400).send('Token Inválido');
+    }
+
+    next();
+  }
 }
 
 export type UserData = {
